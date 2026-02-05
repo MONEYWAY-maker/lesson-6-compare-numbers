0e496508cf54b3de25892c505775f18cad8c108c
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2026-02-04 17:21:57 -0800

    Add files via upload

---
+++ isLessThan.js
@@ -0,0 +1,15 @@
+// Write a function that returns true if a is less than b
+// Example: 4,6 -> true
+//         10,9 -> false
+export function isLessThan(a, b) {
+  // Fill me in!
+}
+
+
+// Call the isLessThan function (try doing this yourself!)
+
+
+// After you've tried your function with different inputs, comment out any
+// lines that print to the console in this file and run the tests!
+
+// can you write this function in a single line?
