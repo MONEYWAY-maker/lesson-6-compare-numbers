facbb95a4302d6725a24fe56c7b980f9ffb3339e
MONEYWAY-maker <lenwoodsbiz@gmail.com>
2026-09-02 21:37:07 -0700

    New updateisLessThan.js

--- isLessThan.js
+++ isLessThan.js
@@ -13,3 +13,16 @@ export function isLessThan(a, b) {
 // lines that print to the console in this file and run the tests!
 
 // can you write this function in a single line?
+// Write a function that returns true if a is less than b
+// Example: 4,6 -> true
+//         10,9 -> false
+export function isLessThan(a, b) {
+  return a < b;   // single‑line solution
+}
+
+// Call the isLessThan function (try doing this yourself!)
+console.log(isLessThan(4, 6));   // true
+console.log(isLessThan(10, 9));  // false
+
+// After you've tried your function with different inputs, comment out any
+// lines that print to the console in this file and run the tests!
